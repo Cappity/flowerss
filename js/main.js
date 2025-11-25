6610d705e806bd00718ccf684bdb19353221b952
Cappity <cap03586@gmail.com>
2025-11-25 23:53:28 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = 'I LOVE YOU AYU'.split('')
+    const titles = 'I LOVE YOU Cuteness'.split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
