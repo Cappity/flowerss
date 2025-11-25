e92040b2d05454cf0ef82302288e18ec17e0b596
Cappity <cap03586@gmail.com>
2025-11-25 23:53:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = 'I LOVE YOU Cuteness'.split('');
+    const titles = 'I LOVE YOU AYU'.split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -10,10 +10,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300);
-      } else {
-        // Start flower rain when the title fully appears
-        startFlowerRain();
+        setTimeout(appendTitle, 300); // 1000ms delay
       }
     }
 
@@ -22,28 +19,3 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
-
-
-// ------------------------------------------
-// FLOWER RAIN EFFECT
-// ------------------------------------------
-
-function startFlowerRain() {
-  const flowers = ["🌸", "💮", "🌺", "🌷", "🌻"];
-
-  setInterval(() => {
-    const flower = document.createElement("div");
-    flower.classList.add("flower");
-    flower.innerText = flowers[Math.floor(Math.random() * flowers.length)];
-
-    flower.style.left = Math.random() * 100 + "vw";
-    flower.style.animationDuration = 3 + Math.random() * 3 + "s";
-    flower.style.fontSize = 20 + Math.random() * 20 + "px";
-
-    document.body.appendChild(flower);
-
-    setTimeout(() => {
-      flower.remove();
-    }, 6000);
-  }, 200);
-}
